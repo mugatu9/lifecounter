4967f2c8ff7c76afb8faeeb46ae5cc2b15eb71fd
Nathan O'Brien <mugatu9@gmail.com>
2020-08-20 16:15:48 -0700

    got buttons working
    Got buttons to properly change HTML in onclick() and added a stylesheet

--- index.js
+++ index.js
@@ -1,50 +1,50 @@
 class Player {
 	
-	constructor(lifeTotal) {
+	constructor(playerNum, lifeTotal) {
+		this.playerNum = playerNum;
 		this.lifeTotal = 0;
 		this.commanderDam = 0;
-		this.wins = 0;
-		this.losses = 0;
 		this.lifeTotal = lifeTotal;
 	}
 
 	incLifeTotal(){
 		this.lifeTotal++;
+		$(".lifeTotal").html(this.lifeTotal);
 	}
 
 	decLifeTotal(){
 		this.lifeTotal--;
+		$( ".lifeTotal" ).html(this.lifeTotal);
 	}
 
 	incCommanderDam() {
 		this.commanderDam++;
+		$( ".commDamage" ).html(this.commanderDam);
 	}
 
 	decCommanderDam() {
 		this.commanderDam--;
-	}
-	incWins() {
-		this.wins++;
-	}
+		$( ".commDamage" ).html(this.commanderDam);
 
-	decWins() {
-		this.wins--;
-	}
-	incLosses() {
-		this.losses++;
-	}
-
-	decLosses() {
-		this.losses--;
 	}
 
 	createPlayerBlock() {
-		$("body").append("<div class=\"player\"> \
-			<button class=\"decLife\">-</button>" + this.lifeTotal + "<button class=\"incLife\">+</button>\
-			<div>");
+		$("body").append("<div class=\"player" + this.playerNum + "\"> \
+			<button class=\"decLife\">-</button> <span class=\"lifeTotal\">" + this.lifeTotal + "</span> <button class=\"incLife\">+</button> <br>\
+			<button class=\"decComm\">-</button> <span class=\"commDamage\">" + this.commanderDam + "</span> <button class=\"incComm\">+</button>\
+			<div>")
 	}
 }
 
 
 
+console.log("da fuq");
+var player1 = new Player(1, 40);
+player1.createPlayerBlock();
+
+//why do I have to put it in an anonymous function? wtf, mate
+$(".decLife").on("click", function(){player1.decLifeTotal();});
+$(".incLife").on("click", function(){player1.incLifeTotal();});
+$(".decComm").on("click", function(){player1.decCommanderDam();});
+$(".incComm").on("click", function(){player1.incCommanderDam();});
 
